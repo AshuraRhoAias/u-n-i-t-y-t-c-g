d2d5b3d4022d266aa4cedbbc93f3e97479e607e3
RHOAIASTEKKEN <antonio.ber04@gmail.com>
2025-06-24 12:23:26 -0600

    update 4.1

--- Assets/WaifuSummoner/Scripts/Developer Data Manager/Data/Efectos/DefeatEffectData.cs
+++ Assets/WaifuSummoner/Scripts/Developer Data Manager/Data/Efectos/DefeatEffectData.cs
@@ -1,10 +1,33 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using WaifuSummoner.Effects;
 
 namespace WaifuSummoner.Effects
 {
+    /// <summary>
+    /// Tipos de filtro disponibles para Defeat.
+    /// </summary>
+    public enum DefeatFilterType
+    {
+        SummonCondition,
+        Role,
+        Element,
+        Reign
+    }
+
+    /// <summary>
+    /// Datos de un único filtro de Defeat.
+    /// </summary>
+    [System.Serializable]
+    public class DefeatFilter
+    {
+        public DefeatFilterType filterType;
+        public SummonCondition summonCondition;
+        public Role roleFilter;
+        public ElementType elementFilter;
+        public Realm reignFilter;
+    }
+
     /// <summary>
     /// Datos para el efecto "Defeat"
     /// </summary>
@@ -43,25 +66,29 @@ namespace WaifuSummoner.GameLogic
                     case DefeatFilterType.SummonCondition:
                         var condition = filter.summonCondition;
                         // Procesar condición de invocación
+                        UnityEngine.Debug.Log("Procesando condición de invocación");
                         break;
 
                     case DefeatFilterType.Role:
                         var role = filter.roleFilter;
                         // Procesar filtro de rol
+                        UnityEngine.Debug.Log("Procesando filtro de rol");
                         break;
 
                     case DefeatFilterType.Element:
                         var element = filter.elementFilter;
                         // Procesar filtro de elemento
+                        UnityEngine.Debug.Log("Procesando filtro de elemento");
                         break;
 
                     case DefeatFilterType.Reign:
                         var reign = filter.reignFilter;
                         // Procesar filtro de reino
+                        UnityEngine.Debug.Log("Procesando filtro de reino");
                         break;
 
                     default:
-                        Debug.LogWarning($"Tipo de filtro no manejado: {filter.filterType}");
+                        UnityEngine.Debug.LogWarning($"Tipo de filtro no manejado: {filter.filterType}");
                         break;
                 }
             }
